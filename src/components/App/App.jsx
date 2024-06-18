15074d628b7aab84d75adc37d8c75806dcc28608
Volodymyr BERESNIEV <105537147+bvv023@users.noreply.github.com>
2024-06-18 19:00:30 +0300

    Initial commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,18 @@
+// src/components/App/App.jsx
+import ContactForm from '../ContactForm/ContactForm';
+import SearchBox from '../SearchBox/SearchBox';
+import ContactList from '../ContactList/ContactList';
+import css from './App.module.css';
+
+const App = () => {
+  return (
+    <div className={css.container}>
+      <h1 className={css.title}>Phonebook</h1>
+      <ContactForm />
+      <SearchBox />
+      <ContactList />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,37 @@
+// src/components/ContactList/ContactList.jsx
+import { useSelector, useDispatch } from 'react-redux';
+import { deleteContact, selectContacts } from '../../redux/contactsSlice';
+import { selectNameFilter } from '../../redux/filtersSlice';
+import Contact from '../Contact/Contact';
+import css from './ContactList.module.css';
+
+const ContactList = () => {
+  const contacts = useSelector(selectContacts);
+  const filter = useSelector(selectNameFilter);
+  const dispatch = useDispatch();
+
+  const getVisibleContacts = () => {
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
+  const filteredContacts = getVisibleContacts();
+
+  return (
+    <ul className={css.list}>
+      {filteredContacts.map(({ id, name, number }) => (
+        <Contact
+          key={id}
+          id={id}
+          name={name}
+          number={number}
+          onDelete={() => dispatch(deleteContact(id))}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default ContactList;
